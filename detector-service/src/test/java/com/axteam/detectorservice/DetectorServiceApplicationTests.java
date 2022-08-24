abc228a5129a7dae861fb7a3a9a73000a46e8bb3
volkov_ds <volkov_ds@nlmk.com>
2022-08-24 14:57:39 +0300

    Changed project tree

---
+++ detector-service/src/test/java/com/axteam/detectorservice/DetectorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axteam.detectorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-sender-service/src/test/java/com/axteam/notificationsenderservice/NotificationSenderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axteam.notificationsenderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSenderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
