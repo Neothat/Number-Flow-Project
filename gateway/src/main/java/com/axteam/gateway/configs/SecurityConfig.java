9f581a380a749dd0c360558b043350f15cc60f3a
volkov_ds <volkov_ds@nlmk.com>
2022-09-06 15:01:24 +0300

    Now only authorized users can make requests

---
+++ gateway/src/main/java/com/axteam/gateway/configs/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.axteam.gateway.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.and()
+				.logout().logoutSuccessUrl("/");
+	}
+
+	@Bean
+	public UserDetailsService users() {
+		UserDetails user = User.builder()
+				.username("bob")
+				.password("{bcrypt}$2a$10$yc/dQH.i7kzC3m/Ih1.Qh.so0O1.tsMtHdNhPK4Fe68g6WpnBTVsa")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+}
